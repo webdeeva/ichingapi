ea110290b83d97fcbc91b4d2a44b6df7842c36cb
webdeeva <tavonia@gmail.com>
2023-08-21 20:04:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.get('/generate', (req, res) => {
     res.json(result);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
