1105ef89b1e94dccb6c88248185bc74fbd49acb5
webdeeva <tavonia@gmail.com>
2023-08-21 21:20:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ const hexagrams = [
     "011101", "101110", "001001", "100100", "110100", "001011", "001101", "101100",
     "110110", "011011", "110010", "010011", "110011", "001100", "010101", "101010"];
 
+const hexagramSymbols = [
+    "\u4DC0", "\u4DC1", "\u4DC2", "\u4DC3", "\u4DC4", "\u4DC5", "\u4DC6", "\u4DC7",
+    "\u4DC8", "\u4DC9", "\u4DCA", "\u4DCB", "\u4DCC", "\u4DCD", "\u4DCE", "\u4DCF",
+    "\u4DD0", "\u4DD1", "\u4DD2", "\u4DD3", "\u4DD4", "\u4DD5", "\u4DD6", "\u4DD7",
+    "\u4DD8", "\u4DD9", "\u4DDA", "\u4DDB", "\u4DDC", "\u4DDD", "\u4DDE", "\u4DDF",
+    "\u4DE0", "\u4DE1", "\u4DE2", "\u4DE3", "\u4DE4", "\u4DE5", "\u4DE6", "\u4DE7",
+    "\u4DE8", "\u4DE9", "\u4DEA", "\u4DEB", "\u4DEC", "\u4DED", "\u4DEE", "\u4DEF",
+    "\u4DF0", "\u4DF1", "\u4DF2", "\u4DF3", "\u4DF4", "\u4DF5", "\u4DF6", "\u4DF7",
+    "\u4DF8", "\u4DF9", "\u4DFA", "\u4DFB", "\u4DFC", "\u4DFD", "\u4DFE", "\u4DFF"
+];
+
+
 const hexagramNumber = (hexagram) => {
     return hexagrams.indexOf(hexagram.join("")) + 1;
 };
@@ -31,11 +43,13 @@ const generateHexagrams = () => {
     const resultingHexagramNumber = hexagramNumber(resulting);
 
     return {
-        initialHexagram: formattedInitial,
+         initialHexagram: formattedInitial,
         resultingHexagram: formattedResulting,
         changingLines: changing,
         initialHexagramNumber,
-        resultingHexagramNumber
+        resultingHexagramNumber,
+        initialHexagramSymbol: hexagramSymbols[initialHexagramNumber - 1],
+        resultingHexagramSymbol: hexagramSymbols[resultingHexagramNumber - 1]
     };
 };
 
